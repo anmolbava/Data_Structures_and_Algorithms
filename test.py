e1bc17d8d2aed3a1c61bedcf81189207b00ff90c
anmolbava <anmolbava96@gmail.com>
2024-04-07 21:31:41 +0100

    test_revise

---
+++ test.py
@@ -0,0 +1,8 @@
+print("Hello testing again")
+len = int(input())
+arr = list(map(int,input().split()))
+for i in range(0,len):
+    print(arr[i],end = ' ')
+
+# please input the value of the array with spaces such as " 1 2 3 4 5"
+    
